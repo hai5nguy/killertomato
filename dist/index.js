6d4c716b35f37f4f6e0c8d0e888b81768e05c983
Hai Nguyen <hai5nguy@gmail.com>
2017-07-11 14:05:45 -0400

    Add more timers

--- dist/index.js
+++ dist/index.js
@@ -1,34 +1,28 @@
 var timer;
 var go = document.getElementById('go');
+var twentyFive = document.getElementById('twenty-five');
+var five = document.getElementById('five');
 var timeElement = document.getElementById('time');
 var timeRemainElement = document.getElementById('timeRemain');
 
 go.addEventListener('click', function () {
-    clearInterval(timer);
-    
-    var time = timeElement.value;
+    var minutes = timeElement.value;
     if (!time.length || isNaN(time)) { return; }
-
-    var minutes = time * 60;
-    showTime(minutes);
-
-    timer = setInterval(function () {
-        minutes--;
-
-        if (minutes <= 0) {
-            clearInterval(timer);
-            showPopup();
-        }
-
-        showTime(minutes);
-        
-    }, 1000);
-
+    startTimer(minutes);
 });
 
-function showTime(minutes) {
-    var m = Math.floor(minutes / 60);
-    var s = String(minutes % 60);
+twentyFive.addEventListener('click', function () {
+    startTimer(25);
+});
+
+five.addEventListener('click', function () {
+    startTimer(5);
+});
+
+
+function showTime(seconds) {
+    var m = Math.floor(seconds / 60);
+    var s = String(seconds % 60);
     s = s.length < 2 ? '0' + s : s;
     timeRemainElement.innerHTML = m + ':' + s;
 }
@@ -36,4 +30,23 @@ function showTime(minutes) {
 function showPopup() {
     var win = window.open("", "Time up", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, width=300, height=200, top=" + (screen.height / 2 - 300) + ", left=" + (screen.width / 2 - 150));
     win.document.body.innerHTML = "killer tomato";
+}
+
+function startTimer(minutes) {
+    clearInterval(timer);
+    
+    var seconds = minutes * 60;
+    showTime(seconds);
+
+    timer = setInterval(function () {
+        seconds--;
+
+        if (seconds <= 0) {
+            clearInterval(timer);
+            showPopup();
+        }
+
+        showTime(seconds);
+        
+    }, 1000);
 }
